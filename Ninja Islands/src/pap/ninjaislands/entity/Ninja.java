f50763c429007016e8f199c9043d105186ced5bf
Carlos Almeida <eternalaiden@gmail.com>
2014-10-13 22:02:16 +0100

    Iniciado a classe do Ninja

---
+++ Ninja Islands/src/pap/ninjaislands/entity/Ninja.java
@@ -0,0 +1,21 @@
+package pap.ninjaislands.entity;
+
+import java.awt.Graphics;
+
+import pap.ninjaislands.resource.DoubleRectangle;
+
+public class Ninja extends DoubleRectangle{
+	
+	public Ninja(double width, double height){
+		setBounds(0, 0, width, height);
+	}
+	
+	public void tick(){
+		
+	}
+	
+	public void render(Graphics g){
+		
+	}
+	
+}
