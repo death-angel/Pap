305e3d28237c5bffa7b3a97656b8e15b04c4c03b
Carlos Almeida <eternalaiden@gmail.com>
2014-10-13 21:41:31 +0100

    Classe para Rectangulos com doubles

---
+++ Ninja Islands/src/pap/ninjaislands/resource/DoubleRectangle.java
@@ -0,0 +1,25 @@
+package pap.ninjaislands.resource;
+
+public class DoubleRectangle {
+	
+	double x;
+	double y;
+	double width;
+	double height;
+	
+	public DoubleRectangle(){
+		setBounds(0, 0, 0, 0);
+	}
+	
+	public DoubleRectangle(double x, double y, double width, double height){
+		setBounds(x, y, width, height);
+	}
+	
+	public void setBounds(double x, double y, double width, double height){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+}
